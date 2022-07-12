d849424a94f413e7b754614d6ce3d5e7e42e2cda
taisuke fujii <tv0420331@gmail.com>
2022-07-12 22:59:04 +0900

    Initial Commit

---
+++ bluetoothTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bluetoothTest
+//
+//  Created by Taisuke Fujii on 2022/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
