e8bb35eed33794fee0aab2458956116719c85b5e
taisuke fujii <tv0420331@gmail.com>
2022-07-14 23:15:02 +0900

    develop initial commit

--- bluetoothTest/ViewController.swift
+++ bluetoothTest/ViewController.swift
@@ -9,10 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func konashiFind(_ sender: Any) {
+        Konashi.find()
+    }
+    @IBOutlet weak var contents: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        //接続先の非表示
+        contents.isHidden = true
+        //イベントハンドラーの登録
+        Konashi.shared().readyHandler = {
+            Konashi.pinMode(KonashiDigitalIOPin.LED2,mode: KonashiPinMode.output)
+            //LED5を光らせる
+            Konashi.digitalWrite(KonashiDigitalIOPin.LED5, value: KonashiLevel.high)
+
+            if(Konashi.isReady()){
+
+               print("-----------------Konashiとの接続が成功しました。--------------------")
+
+                //接続先の表示
+                self.contents.isHidden = false
+                //LED2を光らせる
+                Konashi.digitalWrite(KonashiDigitalIOPin.LED2, value: KonashiLevel.high)
+            }
+
+        }
         // Do any additional setup after loading the view.
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
 
 
 }
